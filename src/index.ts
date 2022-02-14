f3590d1f2c355e940dbb341df19ab31385dc2b23
chanleyou <chanleyou@gmail.com>
2022-02-14 15:37:02 +0800

    use map for duplicate handling

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,11 @@ let possibilities = [...answers];
 for (const g of guesses) {
 	const [guess, hints] = g;
 
-	// order doesn't matter, this is just to handle duplicates
-	// should probably use a map but eh
-	let correct: Letter[] = [];
+	// duplicate handling
+	// prettier-ignore
+	const contains: { [key in Letter]: number } = {
+		a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0,
+	};
 
 	// first handle green vs not green
 	for (let i = 0; i < guess.length; i++) {
@@ -32,7 +34,7 @@ for (const g of guesses) {
 
 		if (hint === 'G') {
 			possibilities = possibilities.filter((w) => w[i] === letter);
-			correct.push(letter);
+			contains[letter]++;
 		} else {
 			possibilities = possibilities.filter((w) => w[i] !== letter);
 		}
@@ -43,16 +45,13 @@ for (const g of guesses) {
 		const hint = hints[i];
 
 		if (hint === 'G') continue; // ignore, handled above
-		if (hint === 'Y') correct.push(letter);
-
-		// handle duplicates
-		const corrects = correct.filter((l) => l === letter).length;
+		if (hint === 'Y') contains[letter]++;
 
 		possibilities = possibilities.filter((w) => {
 			const instances = w.split('').filter((l) => l === letter).length;
 
-			if (hint === 'Y') return instances >= corrects;
-			return instances === corrects;
+			if (hint === 'Y') return instances >= contains[letter];
+			return instances === contains[letter];
 		});
 	}
 }
